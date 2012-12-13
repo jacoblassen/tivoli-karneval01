8eb9fc1e20de10b6f8bbf30069c09a0716347bd1
Jens Peter Schandorff <web@schandorffdesign.dk>
2012-12-13 14:08:32 +0100

    edited program.php and stylesheet.css

--- program.php
+++ program.php
@@ -30,90 +30,87 @@
 <div id="master">
   <div id="frame">
 		<div id="frametop">
-        	<img src="images/rammetop02.png" />
+        	<div class="frametophtag">
+            	<h1>Programplan for karneval i Tivoli</h1>
+            </div><!--frametophtag-->
         </div><!--frametop-->
         
             <div id="sideframewrapper">
             
             	<div id="rammemiddle">
                 
-                <div id="rammecontent">
-                    
-                   <div id="programwrapper">
-                    
-                <h1>Se hvad du kan opleve, når Tivoli åbner dørene for et fantastisk karnevalet</h1>
-                <br	/><br	/>
-              	
-       
-                <h2 onClick="togglefebI()">7. febuar</h2>
-                <span id="febIspan" onClick="togglefebI()">Vis program</span>
-                <br	/> <br	/>
+                	<div id="rammecontent">
+                	    
+                		<div id="programwrapper">
+                   
+                			<h2 onClick="togglefebI()">7. febuar</h2>
+                			<span id="febIspan" onClick="togglefebI()">Vis program</span>
+                			<br	/> <br	/>
                      
-       
-               <p id="febI" class="p1">
-                
-                Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
-                Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
-                Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
-                
-                </p>
-                
-                <br	/><br	/>
-          
-                
-                <h2 onClick="togglefebII()">8. febuar</h2>
-                <span id="febIIspan" onClick="togglefebII()">Vis program</span>
-                <br	/> <br	/>
-                
-                <p id="febII" class="p1">
-                
-                Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
-                Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
-                Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
-                
-                </p> 
-                <br	/><br	/>
-                
-                
-                <h2 onClick="togglefebIII()">9. febuar</h2>
-                <span id="febIIIspan" onClick="togglefebIII()">Vis program</span>
-                <br	/> <br	/>
-                
-                <p id="febIII" class="p1">
-                
-                Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
-                Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
-                Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
-                
-                </p> 
-                <br	/><br	/>
-                
-                
-              	<h2 onClick="togglefebIIII()">10. febuar</h2>
-              	<span id="febIIIIspan" onClick="togglefebIIII()">Vis program</span>
-                <br	/> <br	/>
-                
-                <p id="febIIII" class="p1">
-                
-                Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
-                Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
-                Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
-                
-                </p>
-           
+                   
+                            <p id="febI" class="p1">
+                            
+                            Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
+                            Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
+                            Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
+                            
+                            </p>
+                            
+                            <br	/><br	/>
+                      
+                            
+                            <h2 onClick="togglefebII()">8. febuar</h2>
+                            <span id="febIIspan" onClick="togglefebII()">Vis program</span>
+                            <br	/> <br	/>
+                            
+                            <p id="febII" class="p1">
+                            
+                            Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
+                            Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
+                            Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
+                            
+                            </p> 
+                            <br	/><br	/>
+                            
+                            
+                            <h2 onClick="togglefebIII()">9. febuar</h2>
+                            <span id="febIIIspan" onClick="togglefebIII()">Vis program</span>
+                            <br	/> <br	/>
+                            
+                            <p id="febIII" class="p1">
+                            
+                            Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
+                            Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
+                            Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
+                            
+                            </p> 
+                            <br	/><br	/>
+                            
+                            
+                            <h2 onClick="togglefebIIII()">10. febuar</h2>
+                            <span id="febIIIIspan" onClick="togglefebIIII()">Vis program</span>
+                            <br	/> <br	/>
+                            
+                            <p id="febIIII" class="p1">
+                            
+                            Kl. 12:00 åbnes dørene for karnevalet<br	/><br	/>
+                            Kl. 12:15 kan man komme ind i glassalen og lave sine egen personlige maske<br	/><br	/>
+                            Kl. 12:45 Spiller Stortrommen og klapperne på den store scene<br	/><br	/>
+                            
+                            </p>
+                       
                     
                
-                </div><!--programwrapper-->
+               			</div><!--programwrapper-->
            
-            </div><!--rammecontent-->
+            		</div><!--rammecontent-->
         
-        </div><!--rammemiddle-->
+        		</div><!--rammemiddle-->
             
             
         </div><!--sideframewrapper-->
     	
         <div id="framebottom">
-        	<img src="images/rammebottom02.png" />
         </div><!--framebottom-->
     
     </div><!--frame-->

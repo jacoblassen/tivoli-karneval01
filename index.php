fd41027831edbbc29cecffa503bf2ea1c796dff3
Jens Peter Schandorff <web@schandorffdesign.dk>
2012-12-17 15:58:21 +0100

    finished with index

--- index.php
+++ index.php
@@ -63,7 +63,8 @@
                 <div class="quake-slider-images">
                     <a href="#linkkatten"><img src="images/fastelavn01.jpg" alt="Slå katten af tønden for de små, i Tivoli." /></a>
                     <a href="#linkaarskort"><img src="images/tivoliaarskort.jpg" alt="Tilmeld dig karnevalsfesten i Tivoli med chance for at vinde 2 Tivoli årskort." /></a>
-                    <a href="#linkcostume"><img src="images/udklaedning.jpg" alt="Kom og vær udklædt til Tivolis karneval, der er flotte priser at vinde." /></a>
+                    <a href="#linkcostume"><img src="images/costumes01.jpg" alt="Kom og vær udklædt til Tivolis karneval, der er flotte priser at vinde." /></a>
+                    <a href="#linkudklaedning"><img src="images/udklaedning03.jpg" alt="Tivoli har indgået samarbejde med Udklaedning.dk" /></a>
                 </div>
                 
                 <div class="quake-slider-captions quake-slider-caption-container">
@@ -77,14 +78,22 @@
                     	<h1>Konkurrence</h1>
                         <br />
                         <p class="p1"> - Tilmeld dig Tivolis karneval og vær med i lodtrækningen af gratis årskort!</p>
-            		</div><!--quake-slider-caption-->
-                    <div class="quake-slider-caption">
-                    <h1>Vind</h1>
-                    <p class="p1"> - Pris for de bedst udklædte!</p>
-                    <br />
-                    <p class="p1"> - Kom og vær klædt ud og vind fede premier!</p>
                     </div><!--quake-slider-caption-->
-                </div><!--quake-slider-captions quake-slider-caption-container-->
+                    <div class="quake-slider-caption">
+                        <h1>Vind</h1>
+                        <p class="p1"> - Pris for de bedst udklædte!</p>
+                        <br />
+                        <p class="p1"> - Kom og vær klædt ud og vind fede premier!</p>
+                    </div><!--quake-slider-caption-->
+                    
+                     </div><!--quake-slider-caption-->
+                    <div class="quake-slider-caption">
+                        <h1>Udklaedning.dk</h1>
+                        <p class="p1"> - Spar penge på kostumer hos Udklaedning.dk</p>
+                        <br />
+                        <p class="p1"> - Op til 20% rabat hos Udklaedning.dk.</p>
+                    </div><!--quake-slider-caption-->
+                	</div><!--quake-slider-captions quake-slider-caption-container-->
                 
            	</div>
     	</div><!--featherdiv-->
@@ -128,11 +137,11 @@
                 			<div id="linkaarskort"></div>
                             
                             <div class="nyhedheadline">
-                            <h1>Vind 2 årskort</h1>
+                            <h1>Deltag og vind!</h1>
                             </div><!--nyhedheadline-->
 
                         	<div class="nyhedwrapper">
-                            	<div class="nyhedimage" onclick="tilmeldOpen()"><img src="images/tivoliaarskort02.jpg" alt="Vind et årskort til Tivoli" />
+                            	<div class="nyhedimage" onclick="tilmeldOpen()"><a href="#"><img src="images/tivoliaarskort02.jpg" alt="Vind et årskort til Tivoli" /></a>
                                 </div><!--nyhedimage-->
                                 
                                 <div class="nyhedtext">
@@ -142,7 +151,7 @@
                                 	
                                 
                                 	<div class="timestamp">
-                                    	<p>Dato 01.12.2012</p>
+                                    	<p>Dato 10.12.2012</p>
                                     </div><!--timestamp-->
                                 </div><!--nyhedtext-->
                                 
@@ -151,25 +160,54 @@
                             <hr class="hrgolden" />
                             
                             <div id="linkcostume"></div>
+                            
+                            <div class="nyhedheadline">
+                            <h1>Vær udklædt og vind!</h1>
+                            </div><!--nyhedheadline-->
+                            
                             <div class="nyhedwrapper">
-                            	<div class="nyhedimage"><img src="images/costumes01.jpg" alt="Slå katten af tønden i Tivoli." />
+                            	<div class="nyhedimage"><img src="images/costumes02.jpg" alt="Kom udklædt til karneval i Tivoli og vind!" />
                                 </div><!--nyhedimage-->
                                 
                                 <div class="nyhedtext">
-                                	<p>Fastelavn er en gammel tradition i Danmark og en tid, hvor børn klæder sig ud og har det sjovt. Vi, her i Tivoli, vil gerne være med til at fejre traditionen ved at invitere til at slå katten af tønden.</p>
-                                	<br />
-                                
-                                	<p>Der er nye tønder med slik, fest og farver, hver dag klokken 13.30-14.30 foran koncertsalen.</p>
-                                
-                                	<br />
-                                
-                                	<p>Så hvis du har et eller flere børn mellem 5-10år, så vil i være mere end velkomne til at deltage. Kom og vær med og bliv konge eller dronning for en dag, i Tivoli.</p>
-                                
+                                	
+                                    <p>Vi håber at se så mange, som overhovedet muligt, være udklædt ved vores karneval. Vi håber at i vil gøre lige så meget ud af festen, som vi vil, for så bliver det et brag af et karneval.</p>
+                                    <br />
+                                    
+                                    <p>Vi vil dog ikke nægte adgang til folk uden kostumer, men vi opfordre jer til at slippe jer løs og være med.</p>
+                                    
+                                    <br />
+                                    
+                                    <p>Kom udklædt til karnevallet og få din entrébillet til 50%.</p>
+                                    
                                 	<div class="timestamp">
-                                    	<p>Dato 15.11.2012</p>
+                                    	<p>Dato 07.12.2012</p>
                                     </div><!--timestamp-->
                                 </div><!--nyhedtext-->
                             </div><!--nyhedwrapper-->
+                            
+                            <hr class="hrgolden" />
+                            
+                            <div id="linkudklaedning"></div>
+                            
+                            <div class="nyhedheadline">
+                            <h1>Tivoli og Udklaedning.dk</h1>
+                            </div><!--nyhedheadline-->
+                            
+                            <div class="nyhedwrapper">
+                            	<div class="nyhedimage"><a href="http://www.udklaedning.dk" target="_blank"><img src="images/udklaedning02.jpg" alt="Kom udklædt til karneval i Tivoli og vind!" /></a>
+                                </div><!--nyhedimage-->
+                                
+                                <div class="nyhedtext">
+                                	<p>Tivoli har indgået et samarbejde med Udklaedning.dk. Det har vi gjort for at gøre det nemt og billigt for dig, at få inspiration til et kostume, eller købe et fra deres webshop.</p>                        
+                                    
+                                	<div class="timestamp">
+                                    	<p>Dato 01.12.2012</p>
+                                    </div><!--timestamp-->
+                                </div><!--nyhedtext-->
+                            </div><!--nyhedwrapper-->
+                            
+                            
                 
                 		</div><!--rammemiddle-->
                     
